0a0b3355df2bbaac27a9d434b86df83b3c569267
tsuyoshi.tokuyama <tsuyoshi.tokuyama.p236@dirbato.co.jp>
2024-04-08 18:00:15 +0900

    かくにん

--- Sample/src/sample/ForSample.java
+++ Sample/src/sample/ForSample.java
@@ -3,6 +3,23 @@ package sample;
 public class ForSample {
 
     public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+                // 現在の処理をスキップし、次の繰り返しに進む
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+        for (int i = 2; i <= 100; i+=2) {
+            if (i > 20) {
+                // 繰り返しから抜け出す
+                break;
+            }
+            System.out.println(i);            
+        }
+        
         for (var i = 1; i <= 10; i++) {
             System.out.println(i);
         }
